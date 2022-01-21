24d43bd4c61e59c3167ea606fe7329966ea7ba91
jayabharathi-jb <jayabharathi.s@thoughtworks.com>
2022-01-21 16:16:29 +0530

    Second draft

--- src/test/java/CartTest.java
+++ src/test/java/CartTest.java
@@ -13,4 +13,14 @@ public class CartTest {
         assertEquals(cart.getItems().size(), 1);
         assertEquals(cart.getItems().get(0).getName(), "IPad Pro");
     }
+
+    @Test
+    public void addHeroInkPenToCart() {
+        Cart cart = new Cart();
+
+        cart.addItem(new Item("Hero Ink Pen"));
+
+        assertEquals(cart.getItems().size(), 1);
+        assertEquals(cart.getItems().get(0).getName(), "Hero Ink Pen");
+    }
 }
